749255950436403f125e50a32d987e805032f7c8
Steele Price <steele@thecowboy.ai>
2024-04-27 14:08:51 -0700

    use disko

--- vhosts/x86/builder.nix
+++ vhosts/x86/builder.nix
@@ -10,53 +10,17 @@
     script = with pkgs; ''
       echo 'journalctl -fb -n100 -uinstall' >>~nixos/.bash_history
 
-      set -eux
+      install -D ${./disk-config.nix} /tmp/disk-config.nix
 
-      wait-for() {
-        for _ in seq 10; do
-          if $@; then
-            break
-          fi
-          sleep 1
-        done
-      }
+      sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disk-config.nix
 
-      dev=/dev/nvme0n1
-      dev2=/dev/nvme1n1
-
-      ${utillinux}/bin/sfdisk --wipe=always $dev <<-END
-        label: gpt
-        name=BOOT, size=512MiB, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-        name=NIXOS
-      END
-      mkfs.fat -F 32 -n boot /dev/disk/by-partlabel/BOOT
-
-      sync
-      wait-for [ -b /dev/disk/by-partlabel/BOOT ]
-
-      wait-for mkfs.fat -F 32 -n boot /dev/disk/by-partlabel/BOOT
-
-      wait-for mkfs.ext4 -L nixos /dev/mapper/root
-
-      sync
-      mount /dev/mapper/root /mnt
-
-      mkdir /mnt/boot
-      mkdir /mnt/data
-
-      wait-for mount /dev/disk/by-label/boot /mnt/boot
-
-      ${utillinux}/bin/sfdisk --wipe=always $dev2 <<-END
-        label: gpt
-        name=DATA
-      END
-      mkfs.ext4 -L data /dev/mapper/data
-
-      wait-for mount /dev/disk/by-label/data /mnt/data
+      # do detection better ffs.
+      sleep 20
 
       install -D ${./flake.nix} /mnt/etc/nixos/flake.nix
       install -D ${./configuration.nix} /mnt/etc/nixos/configuration.nix
       install -D ${./hardware-configuration.nix} /mnt/etc/nixos/hardware-configuration.nix
+      install -D ${./containers.nix} /mnt/etc/nixos/containers.nix
 
       ${config.system.build.nixos-install}/bin/nixos-install --flake .#vhost-dev
 
---
+++ vhosts/x86/disk-config.nix
@@ -0,0 +1,51 @@
+{
+  disko.devices = {
+    disk = {
+      sys = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+    disk2 = {
+      data = {
+        device = "/dev/nvme1n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/data";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

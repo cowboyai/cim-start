e27be6afb4b6cd2587e60844ab4630574b98f792
Steele Price <steele@thecowboy.ai>
2024-05-03 14:36:20 -0700

    switch to wired

--- vhosts/x86/vhost-dev/network.nix
+++ vhosts/x86/vhost-dev/network.nix
@@ -22,31 +22,57 @@
 	# network #x for isolated containers
 
 	# this machine has 5 network interfaces
-	# 4 wired and 1 wireless
+	# 2 - 10 Gb wired
+	# 2 - 1 Gb wired
+	# 1 wireless (802.11ac)
 
-	# dhcp or enable settings below
+  # not using wirelesss for now...
+	# using dhcp or enable settings below
 
 	# networking.defaultGateway = "192.168.1.1"; 
 
 	# networking.interfaces.wlp101s0.useDHCP = false;
 	# networking.interfaces.wlp101s0.ipv4.addresses = [
 	# 	{
-	# 		address = "192.168.1.102";
+	# 		address = "192.168.1.199";
 	# 		prefixLength = 24;
 	# 	}
 	# ];
 
-	# networking.interfaces.enp2s0.useDHCP = false;
-	# networking.interfaces.enp2s0.ipv4.addresses = [
-	# 	{
-	# 		address = "192.168.1.101";
-	# 		prefixLength = 24;
-	# 	}
-	# ];
+	networking.interfaces.enp2s0.useDHCP = false;
+	networking.interfaces.enp2s0.ipv4.addresses = [
+		{
+			address = "192.168.1.101";
+			prefixLength = 24;
+		}
+	];
 
+	networking.interfaces.enp0s31f6.useDHCP = false;
+	networking.interfaces.enp0s31f6.ipv4.addresses = [
+		{
+			address = "192.168.1.102";
+			prefixLength = 24;
+		}
+	];
+
+	networking.interfaces.enp23s0f0.useDHCP = false;
+	networking.interfaces.enp23s0f0.ipv4.addresses = [
+		{
+			address = "192.168.1.103";
+			prefixLength = 24;
+		}
+	];
+
+	networking.interfaces.enp23s0f1.useDHCP = false;
+	networking.interfaces.enp23s0f1.ipv4.addresses = [
+		{
+			address = "192.168.1.104";
+			prefixLength = 24;
+		}
+	];
 
 	# the network relies on time...
-	
+
   # TIME
   time.timeZone = "America/Phoenix";
   

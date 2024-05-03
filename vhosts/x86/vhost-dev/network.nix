7d1601432cb9955471e4acb94c8377700c73f545
Steele Price <steele@thecowboy.ai>
2024-05-03 14:37:10 -0700

    clean

--- vhosts/x86/vhost-dev/network.nix
+++ vhosts/x86/vhost-dev/network.nix
@@ -1,43 +1,10 @@
 # network.nix
 {...}: {
-	networking.wireless.enable = true; # Enable wireless networking
-  networking.wireless.networks = {
-    "cim-dev" = {
-      psk = "Unsecure!";
-    };
-  };
-
-	networking.networkmanager.unmanaged = [
-    "*" "except:type:wwan"
-	];
   networking.networkmanager.enable = true;
   
   networking.hostName = "vhost-dev";
   
   networking.firewall.allowedTCPPorts = [ 22 443 ];
-
-	# we have at least 2 networks to manage...
-	# network #1 the LAN
-	# network #2 the containers
-	# network #x for isolated containers
-
-	# this machine has 5 network interfaces
-	# 2 - 10 Gb wired
-	# 2 - 1 Gb wired
-	# 1 wireless (802.11ac)
-
-  # not using wirelesss for now...
-	# using dhcp or enable settings below
-
-	# networking.defaultGateway = "192.168.1.1"; 
-
-	# networking.interfaces.wlp101s0.useDHCP = false;
-	# networking.interfaces.wlp101s0.ipv4.addresses = [
-	# 	{
-	# 		address = "192.168.1.199";
-	# 		prefixLength = 24;
-	# 	}
-	# ];
 
 	networking.interfaces.enp2s0.useDHCP = false;
 	networking.interfaces.enp2s0.ipv4.addresses = [
